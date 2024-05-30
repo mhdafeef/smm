cbe3ac3402eaa4d5e17ec791314b1c859ceb131e
mhdafeef <81076482+mhdafeef@users.noreply.github.com>
2024-05-30 22:03:44 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+const typedTextSpan = document.querySelector(".typing-text");
+        const cursorSpan = document.querySelector(".cursor");
+        
+        const textArray = ["REELS", "VIDEOS", "POSTERS"];
+        const typingDelay = 100;
+        const erasingDelay = 100;
+        const newTextDelay = 1200; // Delay between current and next text
+        let textArrayIndex = 0;
+        let charIndex = 0;
+        
+        function type() {
+          if (charIndex < textArray[textArrayIndex].length) {
+            if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+            typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+            charIndex++;
+            setTimeout(type, typingDelay);
+          } 
+          else {
+            cursorSpan.classList.remove("typing");
+              setTimeout(erase, newTextDelay);
+          }
+        }
+        
+        function erase() {
+            if (charIndex > 0) {
+            if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+            typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+            charIndex--;
+            setTimeout(erase, erasingDelay);
+          } 
+          else {
+            cursorSpan.classList.remove("typing");
+            textArrayIndex++;
+            if(textArrayIndex>=textArray.length) textArrayIndex=0;
+            setTimeout(type, typingDelay + 1100);
+          }
+        }
+        
+        document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+          if(textArray.length) setTimeout(type, newTextDelay + 250);
+        });
+
+/* Scrollreveal Animations
+======================================= */
+
+const sr = ScrollReveal({
+    distance: "100px",
+    duration: 2500,
+    delay: 200,
+    reset: false,
+  });
+  
+  sr.reveal(".typing-1", { origin: "top" });
+  sr.reveal(".img-hom", { origin: "bottom", interval: 200 });
+  sr.reveal(
+    ".img-hom",
+    { origin: "left" }
+  );
+  sr.reveal(
+    ".typing-1",
+    { origin: "right" }
+  );
